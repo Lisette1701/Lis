8d365f037c61daef249260b6368601cc8a269334
Lisette1701 <lisettegomez@liceovvh.cl>
2025-03-11 11:16:55 -0300

    ok

---
+++ JS/02_tipo_variables/script.js
@@ -0,0 +1,15 @@
+const rut = 226152902261231
+let numero =0;
+let decimal = 2.5;
+let texto = "Hola";
+let booleano = true;
+
+let indefinida = underfined;
+
+let array = [1, "Hola", 25];
+let matriz =[[1,2],[3,4]];
+let objeto = {nombre: "Delay", edad:18};
+
+function sumar(a,b){
+    return a + b
+}
